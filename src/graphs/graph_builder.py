c3a7c7f5a59af5dd2a8187ea0a4ea016cd6b4bd2
Puneeth0106 <puneethkumaramudala7@gmail.com>
2026-01-26 09:14:31 -0500

    Deploying in Langgraph studio by creating language.json

--- src/graphs/graph_builder.py
+++ src/graphs/graph_builder.py
@@ -2,6 +2,10 @@ from langgraph.graph import START, END, StateGraph
 from src.llms.groqllm import GroqLLM
 from src.states.blogstate import Blogstate
 from src.nodes.blog_node import blognode
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class Graph_builder:
     def __init__(self,llm):
@@ -23,8 +27,24 @@ class Graph_builder:
         self.graph.add_edge("title_creation", "content_generation")
         self.graph.add_edge("content_generation", END)
 
+
+
     def router_graph(self,use_case):
         if use_case=="topic":
             self.build_topic_graph()
         return self.graph.compile()
         
+
+# --- CLI / STUDIO ENTRY POINT ---
+
+# 1. Setup the LLM
+llm_instance = GroqLLM().groqllm()
+
+# 2. Instantiate your builder
+builder = Graph_builder(llm_instance)
+
+# 3. Execute the build (This sets up nodes and edges)
+builder.build_topic_graph()
+
+# 4. Compile and assign to 'graph' so langgraph.json can find it
+graph = builder.graph.compile()
